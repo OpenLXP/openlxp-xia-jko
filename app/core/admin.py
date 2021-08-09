26987c1bf99ba8b879e6492eb690c370e02bb3a7
SHGODE <SHGODE@DELOITTE.com>
2021-08-09 15:42:11 -0400

    adding a line in admin.py

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,6 +4,7 @@ from .models import XSRConfiguration
 
 # Register your models here.
 
+
 @admin.register(XSRConfiguration)
 class XSRConfigurationAdmin(admin.ModelAdmin):
     list_display = ('source_file',)
